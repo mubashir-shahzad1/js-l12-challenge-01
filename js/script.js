c9d20ec1f8b61d552aa3afc113093a232208b59b
Mubashir Shahzad <ms.dew2552@gmail.com>
2022-07-12 08:31:12 +0500

    added js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImg= async function() {
+    const res= await fetch(" https://picsum.photos/v2/list?limit=100");
+const img = await res.json();
+console.log(img);
+};
+getImg();
